84677fdfffc649a354853e00a59fea995bc329f5
oscardenas17 <oscardenas17@gmail.com>
2022-07-17 08:17:37 -0500

    clase JSX & transpiling & Componentes I

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  // const imagen = <img src="url" alt="imagen" />
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header id="header" className="App-header OtraClase">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button style={{ color: 'red', backgroundColor: 'green', fontSize: '30px'}}>Boton</button>
+        {imagen}
+      </header> */}
+      <Navbar />
     </div>
   );
 }
